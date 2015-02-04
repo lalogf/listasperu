855860c823ff202986b83696b7736f6eb03948f8
lalogf <lalogf@gmail.com>
2015-02-04 10:06:33 -0500

    starting project

---
+++ app/controllers/provincias_controller.rb
@@ -0,0 +1,2 @@
+class ProvinciasController < ApplicationController
+end
---
+++ test/controllers/provincias_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ProvinciasControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
